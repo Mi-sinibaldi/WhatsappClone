f08f2ae97b923094104a59da7e4e222c972f1970
Mi-sinibaldi <mia_araujo92@hotmail.com>
2019-12-27 08:23:04 -0300

    configuracao da base64, ajustes layout e tela configuracao

---
+++ app/src/main/java/com/example/whatsappclone/fragment/ContatosFragments.java
@@ -0,0 +1,29 @@
+package com.example.whatsappclone.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.whatsappclone.R;
+
+public class ContatosFragments extends Fragment {
+
+
+    public ContatosFragments() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contatos_fragments, container, false);
+    }
+
+}
